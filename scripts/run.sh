81e4576bb81c58c901810b1ab6f0bda157163e2d
eve <eveeeon@gmail.com>
2026-08-19 01:14:17 +0100

    set to test subject

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,3 +9,4 @@ THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 "$THIS_DIR/experiment/builder.sh"
 "$THIS_DIR/experiment/post_adapter.sh"
 "$THIS_DIR/experiment/evaluator.sh"
+"$THIS_DIR/experiment/aggregator.sh"
